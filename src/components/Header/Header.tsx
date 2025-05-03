9c1e778f615a17f1ebf78f38c6daf48b16a7f131
Hak0bian <hakobyan7171@gmail.com>
2025-05-03 14:28:01 +0400

    change header photos

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import img1 from "../../assets/1.png"
-import img2 from "../../assets/2.png"
-import img3 from "../../assets/3.png"
+import img2 from "../../assets/2.jpg"
+import img3 from "../../assets/3.jpg"
 import img4 from "../../assets/4.jpg"
 import st from "./Header.module.css"
 
